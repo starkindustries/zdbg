a636642ccb85e469a4714ba440b404640a574960
Zion <zionperez@gmail.com>
2025-05-12 21:56:57 -0700

    Add Zigzag Conversion solution implementation in 6-zigzag-conversion.py - a leetcode problem

---
+++ 6-zigzag-conversion.py
@@ -0,0 +1,21 @@
+
+
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        conversion = [""] * numRows
+        print(conversion)
+        idx = 0
+        # Ex: numRows 4: 0 1 2 3 2 1 repeat
+        idx_sequence = list(range(numRows)) + list(range(numRows-2, 0, -1))
+        while idx < len(s):
+            for j in idx_sequence:
+                conversion[j] += s[idx]
+                idx += 1
+                if idx >= len(s):
+                    break
+        return "".join(conversion)
+
+s = "PAYPALISHIRING"
+solve = Solution()
+result = solve.convert(s, 4)
+print(result)
